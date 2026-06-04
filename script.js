a4fad9b775663a958dce673940f9a5bcb75b2f51
Sks <124144700+sks123git@users.noreply.github.com>
2026-06-04 18:04:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function daysOfYear(year) {
+	let yr = parseInt(year);
+	if((yr%4==0 && yr%100!==0) || yr%400==0) {
+	    console.log(366)
+	} else {
+	    console.log(365)
+	}
+}
